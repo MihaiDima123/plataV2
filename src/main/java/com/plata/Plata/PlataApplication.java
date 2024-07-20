e787319da963f3d772445b0647f0e869ef5e9167
Mihai <dimamihaimail@gmail.com>
2024-07-20 14:34:52 +0300

    first commit

---
+++ src/main/java/com/plata/Plata/PlataApplication.java
@@ -0,0 +1,11 @@
+package com.plata.Plata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PlataApplication.class, args);
+	}
+}
