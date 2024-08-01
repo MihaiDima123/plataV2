03d0c3a38c51f8c51921665dee542aface0cdb17
Mihai <dimamihaimail@gmail.com>
2024-08-01 23:37:49 +0300

    remove for now the oauth2

--- app/backend/src/main/java/com/plata/Plata/configuration/SecurityConfiguration.java
+++ app/backend/src/main/java/com/plata/Plata/configuration/SecurityConfiguration.java
@@ -5,11 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -17,21 +13,14 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityWebFilterChain(HttpSecurity http) throws Exception {
-        var builder = http.authorizeHttpRequests(authorization -> {
+        var builder = http.authorizeHttpRequests(authorization ->
             authorization
                     .anyRequest()
-                    .authenticated();
-        })
-        .oauth2Login(withDefaults())
-        .oauth2ResourceServer(customizer -> customizer.jwt(withDefaults()))
+                    .authenticated()
+        )
         .cors(AbstractHttpConfigurer::disable)
         .csrf(AbstractHttpConfigurer::disable);
 
         return builder.build();
-    }
-
-    @Bean
-    public JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withJwkSetUri("https://www.googleapis.com/oauth2/v3/certs").build();
     }
 }
