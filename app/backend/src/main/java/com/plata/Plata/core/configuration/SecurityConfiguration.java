70b43ed34ecad2b86b63462185503a0ac35c28d9
Mihai <dimamihaimail@gmail.com>
2024-08-10 13:05:25 +0300

    m-10: add the custom filter before the username and password authentication

--- app/backend/src/main/java/com/plata/Plata/core/configuration/SecurityConfiguration.java
+++ app/backend/src/main/java/com/plata/Plata/core/configuration/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.plata.Plata.core.configuration;
 
+import com.plata.Plata.core.configuration.filters.JwtAuthenticationServletFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,22 +9,27 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    private final JwtAuthenticationServletFilter jwtAuthenticationServletFilter;
+
+    public SecurityConfiguration(JwtAuthenticationServletFilter jwtAuthenticationServletFilter) {
+        this.jwtAuthenticationServletFilter = jwtAuthenticationServletFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityWebFilterChain(HttpSecurity http) throws Exception {
-        var builder = http.authorizeHttpRequests(authorization ->
-            authorization
-                    .requestMatchers("api/v1/auth/**")
-                    .permitAll()
-                    .anyRequest()
-                    .authenticated()
-        )
-        .cors(AbstractHttpConfigurer::disable)
-        .csrf(AbstractHttpConfigurer::disable);
+        var builder = http
+            .cors(AbstractHttpConfigurer::disable)
+            .csrf(AbstractHttpConfigurer::disable)
+            .addFilterBefore(jwtAuthenticationServletFilter, UsernamePasswordAuthenticationFilter.class)
+            .authorizeHttpRequests(authorization -> authorization
+                    .requestMatchers("api/v1/auth/**").permitAll()
+                    .anyRequest()   .authenticated()
+        );
 
         return builder.build();
     }
