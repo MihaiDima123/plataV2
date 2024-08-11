3fefa93cdad500fdbebbc5c0d9f3ef882def4619
Mihai <dimamihaimail@gmail.com>
2024-08-11 19:07:43 +0300

    configure cors

--- app/backend/src/main/java/com/plata/Plata/core/configuration/SecurityConfiguration.java
+++ app/backend/src/main/java/com/plata/Plata/core/configuration/SecurityConfiguration.java
@@ -10,6 +10,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -21,9 +26,22 @@ public class SecurityConfiguration {
     }
 
     @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
+    @Bean
     public SecurityFilterChain securityWebFilterChain(HttpSecurity http) throws Exception {
         var builder = http
-            .cors(AbstractHttpConfigurer::disable)
+            .cors(cors -> cors.configure(http))
             .csrf(AbstractHttpConfigurer::disable)
             .addFilterBefore(jwtAuthenticationServletFilter, UsernamePasswordAuthenticationFilter.class)
             .authorizeHttpRequests(authorization -> authorization
